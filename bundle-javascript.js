feed7384e7a80f565fa4791900353ebd5c358c01
rubonics <rubenrodriguez+github@gmail.com>
2022-01-18 12:31:35 -0500

    adds bundle variable to define the domain of the server to enable connecting to the websocket server from the local network

---
+++ bundle-javascript.js
@@ -0,0 +1,26 @@
+const { networkInterfaces } = require('os')
+
+const nis = networkInterfaces()
+
+let ip = ''
+
+Object.keys(nis).forEach((niKey) => {
+  const ni = nis[niKey]
+  ni.forEach((niSpec) => {
+    if (niSpec.address.startsWith('192')) {
+      ip = niSpec.address
+    }
+  })
+})
+
+const browserify = require('browserify')
+const envify = require('envify/custom')
+const fs = require('fs')
+ 
+const b = browserify('ui.js')
+const output = fs.createWriteStream('public/bundle.js')
+ 
+b.transform(envify({
+  SERVER_DOMAIN: ip,
+}))
+b.bundle().pipe(output)
